b8c55b121ffb7499f81a3703c7bf8fe1f170bc18
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-11-19 13:17:37 +0700

    update data iot

--- app/Http/Controllers/LokasiSawahController.php
+++ app/Http/Controllers/LokasiSawahController.php
@@ -10,34 +10,35 @@ use Illuminate\Support\Facades\Http;
 
 class LokasiSawahController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('/pages/formlokasisawah');
-        
     }
 
-    public function tambahlokasi(request $request){
+    public function tambahlokasi(request $request)
+    {
         $lokasi_latitude = $request->input('lokasi_latitude');
         $lokasi_longitude = $request->input('lokasi_longitude');
         $kabupaten = $request->input('kabupaten');
         $lokasi_keterangan = $request->input('lokasi_keterangan');
         $id_iot = $request->input('id_iot');
-        
+
         // $client = new Client();
         // $api_url = "http://compute.dinus.ac.id:900/api/tambah/datalokasi";
         $post = Http::post('http://compute.dinus.ac.id:900/api/tambah/datalokasi', [
-        // $res = $client->post($api_url. [
-        //     'json' =>     
+            // $res = $client->post($api_url. [
+            //     'json' =>     
             'lokasi_latitude' => (string)$lokasi_latitude,
-                'lokasi_longitude' => (string)$lokasi_longitude,
-                'kabupaten' => (string)$kabupaten,
-                'lokasi_keterangan' => (string)$lokasi_keterangan,
-                'id_iot' => (int)$id_iot
+            'lokasi_longitude' => (string)$lokasi_longitude,
+            'kabupaten' => (string)$kabupaten,
+            'lokasi_keterangan' => (string)$lokasi_keterangan,
+            'id_iot' => (int)$id_iot
         ]);
-            
-            // $tanggal = $date->format('Y-m-d H:i:s');
-            // return response()->json(['massage'=> 'insert data success', 'data' => $tambahlokasi]);
-        return redirect('kirimlokasi')->with(['success' => 'Data Berhasil Terinput']);}
-            // return redirect('tambahlokasi')->with(['success' => 'Pesan Berhasil']);
+
+        // $tanggal = $date->format('Y-m-d H:i:s');
+        // return response()->json(['massage'=> 'insert data success', 'data' => $tambahlokasi]);
+        // return redirect('kirimlokasi')->with(['success' => 'Data Berhasil Terinput']);
+        return redirect('/formlokasisawah')->with(['success' => 'Data Berhasil Terinput']);
+    }
+    // return redirect('tambahlokasi')->with(['success' => 'Pesan Berhasil']);
 }
-
-
