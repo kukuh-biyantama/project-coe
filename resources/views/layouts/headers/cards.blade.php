ea1d790696065e88fcbc38525ba6462873f004bb
muhammadriza007 <muhammadriza664@gmail.com>
2022-11-29 10:46:37 +0700

    update dashboard v2

--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -17,19 +17,31 @@
 
                     fjs.parentNode.insertBefore(js, fjs);
                 })(document, 'script', 'tomorrow-sdk');
-            </script>
+                </script>
 
-            <div class="tomorrow" data-location-id="056965" data-language="EN" data-unit-system="METRIC" data-skin="light" data-widget-type="aqiPollen" style="padding-bottom:22px;position:relative;">
-                <a href="https://www.tomorrow.io/weather/" rel="nofollow noopener noreferrer" target="_blank" style="position: absolute; bottom: 0; transform: translateX(-50%); left: 50%;">
-                </a>
-            </div>
-            {{-- <div class="container">
+                <div class="tomorrow"
+                   data-location-id="056965"
+                   data-language="EN"
+                   data-unit-system="METRIC"
+                   data-skin="light"
+                   data-widget-type="aqiPollen"
+                   style="padding-bottom:22px;position:relative;"
+                >
+                  <a
+                    href="https://www.tomorrow.io/weather/"
+                    rel="nofollow noopener noreferrer"
+                    target="_blank"
+                    style="position: absolute; bottom: 0; transform: translateX(-50%); left: 50%;"
+                  >
+                  </a>
+                </div>
+                {{-- <div class="container">
                     <div class="row">
                       <div class="col-4">.col-4<br>Since 9 + 4 = 13 &gt; 12, this 4-column-wide div gets wrapped onto a new line as one contiguous unit.</div>
                       <div class="col-6">.col-6<br>Subsequent columns continue along the new line.</div>
                     </div>
                 </div> --}}
-            {{-- <div class="col-xl-4 col-lg-6 pt-2">
+                {{-- <div class="col-xl-4 col-lg-6 pt-2">
                     <div class="card card-stats bg-white mb-4 mb-xl-0 btn btn-secondary">
                         <div class="card-body pb-2">
                             <div class="row align-items-center">
@@ -77,8 +89,8 @@
                 </div> --}}
             <div class="row pt-2">
                 <div class="container-xl-4 col-lg-4">
-                    <div class="card card-stats mb-4 mb-xl-0 btn btn-secondary">
-                        <a href="/add-blog-post-form">
+                    <div class="card card-stats mb-4 mb-xl-0 btn btn-secondary" >
+                        <a href="/formlokasisawah/">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col">
@@ -97,21 +109,22 @@
                 <div class="container-xl-8 col-lg-8">
                     <div class="card card-stats mb-2 mb-xl-0 btn btn-secondary">
                         {{-- <a href="/formpenanamanbawang/"> --}}
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col">
-                                    <h2 class="card-title text-uppercase text-muted mb-3">Lokasi Pak Farhan</h2>
-                                </div>
-                                {{-- <div class="col">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <h2 class="card-title text-uppercase text-muted mb-3">Lokasi Pak Farhan</h2>
+                                    </div>
+                                    {{-- <div class="col">
                                         <div class="icon icon-shape bg-info text-white rounded-circle shadow">
                                             <i class="fa fa-location-arrow"></i>
                                         </div>
                                     </div> --}}
+                                </div>
                             </div>
-                        </div>
                         {{-- </a> --}}
                     </div>
                 </div>
+                <div class="container-xl-4 col-lg-4 pt-2">
                     <div class="card card-stats bg-white mb-4 mb-xl-0 btn btn-secondary">
                         <div class="card-body">
                             <div class="row align-items-center">
@@ -157,6 +170,8 @@
                             </div>
                         </div>
                     </div>
+                    <div class="card card-stats mb-4 mb-xl-0 mt-2 btn btn-secondary">
+                        <a href="/formpenanamanbawang/">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col">
@@ -183,7 +198,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="icon icon-shape text-black rounded-circle shadow">
-                                        <i class="fas fa-calendar" style="width 100px, height 100px"></i>
+                                        <i class="fas fa-calendar" style="width 100px, height 100px" ></i>
                                     </div>
                                 </div>
                                 <div class="row">
@@ -220,7 +235,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="icon icon-shape text-black rounded-circle shadow">
-                                        <i class="fas fa-calendar" style="width 100px, height 100px"></i>
+                                        <i class="fas fa-calendar" style="width 100px, height 100px" ></i>
                                     </div>
                                 </div>
                                 <div class="row">
@@ -288,7 +303,8 @@
                         <div class="card-body">
                             <div class="row align-items-center">
                                 <div class="col">
-                                    <h2 class="mb-0 text-black text-center">Saat ini Anda Berada Di Cluster {{$petanicluster}}</h2>
+                                    <h2 class="mb-0 text-black text-center">Saat ini Anda Berada Di Cluster</h2>
+                                    <h2 class="mb-0 text-black text-center"> {{$petanicluster}}</h2>
                                     <h2 class="mb-0 text-black text-center">Petani {{$namapetani}}</h2>
                                 </div>
                             </div>
