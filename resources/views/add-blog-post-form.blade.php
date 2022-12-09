bbf2bf84c1c1651e8e66c8ef427803db7ace89b8
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-12-09 17:29:33 +0700

    update

--- resources/views/add-blog-post-form.blade.php
+++ resources/views/add-blog-post-form.blade.php
@@ -2,77 +2,71 @@
 {{-- @include('layouts.navbars.navbar') --}}
 @section('content')
 
-    <div class="container-fluid mt--7">
-        <h2 class="text-center mb-4 mt-4 text-light">Form Kirim Lokasi</h2>
+<div class="container-fluid mt--7">
+  <h2 class="text-center mb-4 mt-4 text-light">Form Kirim Lokasi</h2>
 
-        <div class="container mt-4">
-          @if(session('status'))
-          <div class="alert alert-success">
-            {{ session('status') }}
-          </div>
-          @endif
-          <div class="card">
-            <div class="card-header text-center font-weight-bold">
-            </div>
-            <div class="card-body">
-              <form name="add-blog-post-form" id="add-blog-post-form" method="post" action="{{url('store-form')}}">
-                @csrf
-                {{-- <div class="form-group">
+  <div class="container mt-4">
+    @if(session('status'))
+    <div class="alert alert-success">
+      {{ session('status') }}
+    </div>
+    @endif
+    <div class="card mt-5">
+      <div class="card-header text-center font-weight-bold">
+      </div>
+      <div class="card-body">
+        <form name="add-blog-post-form" id="add-blog-post-form" method="post" action="{{url('store-form')}}">
+          @csrf
+          {{-- <div class="form-group">
                 <label for="exampleInputEmail1">id</label>
                 <input type="id" id="id" name="id" class="form-control" required="">
               </div> --}}
-                <div class="form-group">
-                  <label for="exampleInputEmail1">id_iot</label>
-                  <input type="id_iot" id="id_iot" name="id_iot" class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">kabupaten</label>
-                  <input type="kabupaten" id="kabupaten" name="kabupaten" class="form-control" required="">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">lokasi_latitude</label>
-                  <input type="lokasi_latitude" id="lokasi_latitude" name="lokasi_latitude" class="form-control">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputEmail1">lokasi_longitude</label>
-                  <input type="lokasi_longitude" id="lokasi_longitude" name="lokasi_longitude" class="form-control">
-                </div>
-                <label for="exampleInputEmail1">lokasi Keterangan</label>
-                <select name="lokasi_keterangan" id="lokasi_keterangan">
-                  <option value="1">sawah 1</option>
-                  <option value="2">sawah 2</option>
-                  <option value="3">sawah 3</option>
-                  <option value="4">sawah 4</option>
-                </select>
-                <br><br>
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-            </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">id_iot</label>
+            <input type="id_iot" id="id_iot" name="id_iot" class="form-control">
           </div>
-          <a href="/home"><button type="button" class="btn btn-primary btn-lg">Kembali</button></a>
-        </div>
-      </body>
-      
-      </html>
-@endsection
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-</script>
+          <div class="form-group">
+            <label for="exampleInputEmail1">kabupaten</label>
+            <input type="kabupaten" id="kabupaten" name="kabupaten" class="form-control" required="">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">lokasi_latitude</label>
+            <input type="lokasi_latitude" id="lokasi_latitude" name="lokasi_latitude" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">lokasi_longitude</label>
+            <input type="lokasi_longitude" id="lokasi_longitude" name="lokasi_longitude" class="form-control">
+          </div>
+          <label for="exampleInputEmail1">lokasi Keterangan</label>
+          <select name="lokasi_keterangan" id="lokasi_keterangan">
+            <option value="1">sawah 1</option>
+            <option value="2">sawah 2</option>
+            <option value="3">sawah 3</option>
+            <option value="4">sawah 4</option>
+          </select>
+          <br><br>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+    <a href="/home"><button type="button" class="btn btn-primary btn-lg">Kembali</button></a>
+  </div>
+  </body>
 
-<!-- script jquery cdn -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  </html>
+  @endsection
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
 
-<!-- script sweetalert -->
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <!-- script jquery cdn -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-<!-- script toastr -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
-    integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <!-- script sweetalert -->
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+  <!-- script toastr -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
-<footer class="py-5 bg-gradient-primary">
-</footer>
-
-  
+  <footer class="py-5 bg-gradient-primary">
+  </footer>
--- resources/views/pages/penanamanbawang/datapenanamanbawang.blade.php
+++ resources/views/pages/penanamanbawang/datapenanamanbawang.blade.php
@@ -2,21 +2,7 @@
 {{-- @include('layouts.navbars.navbar') --}}
 @section('content')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- toastr -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <title>Data Kegiatan Penanaman Bawang</title>
-</head>
-
-<body>
+<div class="container-fluid mt--7">
     <h2 class="text-center mb-4 mt-4">Data Kegiatan Penanaman Bawang</h2>
 
     <!-- dev container untuk mengatur jarak tampilan -->
@@ -26,7 +12,7 @@
             <a href="/home" type="button" class="btn btn-primary">Kembali</a>
         </div>
         <div class="row">
-            <table class="table table-bordered">
+            <table class="table-responsive table-bordered">
                 <thead>
                     <tr>
                         <!-- <th scope="col">Penanaman ID</th> -->
@@ -87,10 +73,8 @@
     <!-- script toastr -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     {{-- </body> --}}
-    </div>
-    @endsection
+</div>
+@endsection
 
-    <footer class="py-5 bg-gradient-primary">
-    </footer>
-
-    </html>
+<footer class="py-5 bg-gradient-primary">
+</footer>
